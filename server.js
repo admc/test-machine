b191c0be62a3da541bc97693cf8d3f15170a3ab8
Adam Christian <adam.christian@gmail.com>
2010-12-21 00:40:37 -0800

    Making code work for general use

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
+/*
+Copyright 2010, Sauce Labs
 
-/**
- * Module dependencies.
- */
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ 
+ Authors:
+ Adam Christian - adam.christian@gmail.com
+*/
+
+// Module deps
 var express = require('express');
 var sys = require('sys');
 var soda = require('soda')
   , assert = require('assert');
 var request = require('request')
   , jsdom = require('jsdom');  
-var http = require('http'),  
-    io = require('socket.io')
+var http = require('http')
+  , io = require('socket.io')
 
 var app = module.exports = express.createServer();
 
 // Configuration
-app.configure(function(){
+app.configure(function() {
     app.set('views', __dirname + '/views');
     app.use(express.bodyDecoder());
     app.use(express.methodOverride());
@@ -24,11 +39,11 @@ app.configure(function(){
     app.use(express.staticProvider(__dirname + '/public'));
 });
 
-app.configure('development', function(){
+app.configure('development', function() {
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.configure('production', function(){
+app.configure('production', function() {
    app.use(express.errorHandler()); 
 });
 
@@ -44,8 +59,6 @@ app.get('/', function(req, res) {
 var runTest = function(test, url, client) {   
   var browser = soda.createSauceClient({
       'url': url
-    , 'username': 'admc'
-    , 'access-key': 'a45f1897-8194-400c-81a7-2fa336a974a2'
     , 'os': 'Linux'
     , 'browser': 'firefox'
     , 'browser-version': '3.'
@@ -54,14 +67,23 @@ var runTest = function(test, url, client) {
     , 'name': 'Test Machine'
   });
   
-  browser.on('command', function(cmd, args){
+  browser.on('command', function(cmd, args) {
     if (cmd != 'getNewBrowserSession') {
-      client.send( JSON.stringify({task:"showCmd", cmd:cmd, args:args.join(', ')}) );
+      client.send( JSON.stringify({
+        task:"showCmd", 
+        cmd:cmd, 
+        args:args.join(', ')}) 
+      );
     }
     else {
-      client.send( JSON.stringify({task:"showCmd", cmd:"Establishing a connection", args:""}) );
+      client.send( JSON.stringify({
+          task:"showCmd", 
+          cmd:"Establishing a connection", 
+          args:""
+        }) 
+      );
     }
-    //console.log(' \x1b[33m%s\x1b[0m: %s', cmd, args.join(', '));
+    console.log(' \x1b[33m%s\x1b[0m: %s', cmd, args.join(', '));
   });
   
   browser
@@ -69,13 +91,22 @@ var runTest = function(test, url, client) {
     .session()
     .setTimeout(20000);
     
-  client.send( JSON.stringify({task:"showCmd", cmd:"Test Started", args:""}) );
+  client.send( JSON.stringify({
+      task:"showCmd", 
+      cmd:"Test Started", 
+      args:""
+    }) 
+  );
   
-  for (var i=0;i<test.length;i++){
+  // Add each of the actions to soda
+  for (var i=0;i<test.length;i++) {
     var obj = test[i];
     browser.and(function(browser) {
       if (obj.text){
-        browser[obj.method](obj.value.toString(), obj.text);
+        browser[obj.method](
+          obj.value.toString(), 
+          obj.text
+        );
       }
       else {
         browser[obj.method](obj.value.toString());
@@ -86,15 +117,28 @@ var runTest = function(test, url, client) {
   browser
     .testComplete()
     .end(function(err){
-      client.send( JSON.stringify({task:"showCmd", cmd:"Test Done", args:"", sid:this.sid}) );
-      //console.log("done");
+      client.send( JSON.stringify({
+          task:"showCmd", 
+          cmd:"Test Done", 
+          args:"", 
+          sid:this.sid
+        }) 
+      );
     });
 };
 
 var createTest = function(url, client) {
   var page = [];
-  page.push({method:"open", value:url, attrib:"url"});
-  page.push({method:"waitForPageToLoad", value:20000, attrib:false});
+  page.push({
+    method:"open", 
+    value:url, 
+    attrib:"url"
+  });
+  page.push({
+    method:"waitForPageToLoad", 
+    value:20000, 
+    attrib:false
+  });
 
   var queryForTag = function(w, tag, attrib, method) {
     w.jQuery(tag).each(function (index, value) {
@@ -113,11 +157,14 @@ var createTest = function(url, client) {
       else { actionObj.attrib = attrib; }
       
       if (tag == ":text:visible")  {
-        page.push({method:"type", value:value[actionObj.attrib], attrib:actionObj.attrib, text:"testing"});
+        page.push({
+          method:"type", 
+          value:value[actionObj.attrib], 
+          attrib:actionObj.attrib, 
+          text:"testing"
+        });
         
         actionObj.text = "testing";
-        //page.push({method:"verifyValue", value:{loc:value.value, text:v}, attrib:false});
-        //page.push({method:"type", value:value.value, attrib:attrib, text:"testing"});
       }
       
       var clean = value[actionObj.attrib]
@@ -125,7 +172,9 @@ var createTest = function(url, client) {
         clean = value[actionObj.attrib].replace(/<.*?>/g, '');
         clean = clean.replace(/^\s+|\s+$/g, '');
       }
-      if (actionObj.attrib && (value.type != "hidden")) {
+      if (actionObj.attrib && 
+          (value.type != "hidden")) {
+            
         if (tag == "a:visible") {
           clean = "link="+clean;
         }
@@ -148,25 +197,32 @@ var createTest = function(url, client) {
         queryForTag(window, 'h1:visible', 'innerHTML', 'verifyTextPresent');
         queryForTag(window, 'h2:visible', 'innerHTML', 'verifyTextPresent');
         queryForTag(window, 'h3:visible', 'innerHTML', 'verifyTextPresent');
-        client.send( JSON.stringify({task:"showTest", page:page, url:url}) );
+        client.send( JSON.stringify({
+          task:"showTest", 
+          page:page, 
+          url:url}) 
+        );
       })
     }
     else {
-      client.send( JSON.stringify({task:"showError", error:"The provided URL was unaccessible."}) );
+      client.send( JSON.stringify({
+        task:"showError", 
+        error:"The provided URL was unaccessible."}) 
+      );
     }
   })
 };
 
-// Only listen on $ node app.js
-
+// Listen
 if (!module.parent) {
-  app.listen(80);
-  //console.log("Express server listening on port %d", app.address().port)
+  app.listen(process.env.TEST_MACHINE_PORT);
+  console.log("Express server listening on port %d", app.address().port)
 }
 
 var io = io.listen(app)
   , buffer = [];
-  
+
+//socket.io
 io.on('connection', function(client) {
   //client.broadcast({ announcement: client.sessionId + ' connected' });
   
